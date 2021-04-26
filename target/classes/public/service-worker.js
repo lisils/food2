672293e952d5d2eb5d72ecde9036bbed2210f260
Si Li <lisztlee@live.com>
2021-04-26 16:19:44 -0700

    deploy test

--- target/classes/public/service-worker.js
+++ target/classes/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.86f1e525ff03d28ba209ab147f0017b9.js"
+  "/precache-manifest.87428324accf05a51fb8a137f566ab48.js"
 );
 
 self.addEventListener('message', (event) => {
